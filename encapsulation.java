1a79d1cb922195da8ab1a3f8dd26f468fe88af95
Deepak <dcentral56@gmail.com>
2026-05-06 17:12:55 +0530

    Changes to github

--- encapsulation.java
+++ encapsulation.java
@@ -19,7 +19,7 @@ class test {
     public static void main(String[] args)
     {
         encapsulation n = new encapsulation();
-        n.setname("deepak");
+        n.setname("github");
         System.out.println(n.getname());
     }
 }
